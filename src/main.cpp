9788f1cc3d946110daa1ae74c471cd69966d4397
Sascha Peilicke <sasch.pe@gmx.de>
2009-11-12 11:12:23 +0100

    Changed to GPLv2 license and removed old Qt stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,25 @@
 /*
     Copyright (c) 2009 Sascha Peilicke <sasch.pe@gmx.de>
 
-    Permission is hereby granted, free of charge, to any person
-    obtaining a copy of this software and associated documentation
-    files (the "Software"), to deal in the Software without
-    restriction, including without limitation the rights to use,
-    copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following
-    conditions:
+    This application is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Library General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version.
 
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
+    This application is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+    License for more details.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
+    You should have received a copy of the GNU Library General Public License
+    along with this application; see the file COPYING.LIB.  If not, write to the
+    Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+    02110-1301, USA.
 */
 
+#include "boost/threadpool.hpp"
 #include "mergesort.h"
 #include "quicksort.h"
-#include "boost/threadpool.hpp"
 
 #include <boost/bind.hpp>
 #include <boost/lexical_cast.hpp>
@@ -51,21 +45,30 @@ void log(const string &type, const string &name, int size, double time)
 }
 
 template <typename T>
-void measure_std_sort(const string &type, vector<T> data)
-{
-    boost::timer timer;
-    sort(data.begin(), data.end());
-    double elapsed = timer.elapsed();
-    log(type, "std_sort", data.size(), elapsed);
-}
-
-template <typename T>
 void measure_merge_sort(const string &type, vector<T> data)
 {
     boost::timer timer;
     merge_sort(data.begin(), data.end(), less_than<T>);
     double elapsed = timer.elapsed();
     log(type, "merge_sort", data.size(), elapsed);
+}
+
+template <typename T>
+void measure_quick_sort(const string &type, vector<T> data)
+{
+    boost::timer timer;
+    quick_sort(data.begin(), data.end(), less_than<T>);
+    double elapsed = timer.elapsed();
+    log(type, "quick_sort", data.size(), elapsed);
+}
+
+template <typename T>
+void measure_std_sort(const string &type, vector<T> data)
+{
+    boost::timer timer;
+    std::sort(data.begin(), data.end());
+    double elapsed = timer.elapsed();
+    log(type, "std_sort", data.size(), elapsed);
 }
 
 int main(int argc, char *argv[])
@@ -106,16 +109,18 @@ int main(int argc, char *argv[])
     vector<int> data;
     for (unsigned int i = min_size; i <= max_size; i++) {
         data.push_back(dice());
-        tp.schedule(boost::bind(measure_std_sort<int>, "random",  data));
         tp.schedule(boost::bind(measure_merge_sort<int>, "random", data));
+        tp.schedule(boost::bind(measure_quick_sort<int>, "random", data));
+        tp.schedule(boost::bind(measure_std_sort<int>, "random",  data));
     }
 
     cout << "Measure performance for already sorted data..." << endl;
     data.clear();
     for (unsigned int i = min_size; i <= max_size; i++) {
         data.push_back(i);
-        tp.schedule(boost::bind(measure_std_sort<int>, "sorted", data));
         tp.schedule(boost::bind(measure_merge_sort<int>, "sorted", data));
+        tp.schedule(boost::bind(measure_quick_sort<int>, "random", data));
+        tp.schedule(boost::bind(measure_std_sort<int>, "sorted", data));
     }
     return 0;
 }
